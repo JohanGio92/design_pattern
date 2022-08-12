e1249264983e6f00c3793a80ccc74389ad3638ab
Johan Revilla <johan_revilla_92@hotmail.com>
2022-08-12 17:51:52 -0300

    test constructor this super and static

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,16 @@
 package main;
 
+import persona.Alumno;
+import persona.Persona;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		Persona persona = new Persona("johan", "revilla");
+		Alumno alumno = new Alumno("valeria", "saavedra", "95698");
+		alumno.setNombre("omar");
 	}
 
 }
