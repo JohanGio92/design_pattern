0c467f20e98ccc27a1dbc6edd31b5a4470d64448
Johan Revilla <johan_revilla_92@hotmail.com>
2022-08-12 17:51:52 -0300

    test constructor this super and static

---
+++ src/persona/Alumno.java
@@ -0,0 +1,32 @@
+package persona;
+
+public class Alumno extends Persona {
+	
+	private String padron;
+
+	public Alumno(String nombre, String apellido, String padron) {
+		super(nombre, apellido);
+		this.padron = padron;
+	}
+
+	public String getPadron() {
+		return padron;
+	}
+
+	public void setPadron(String padron) {
+		this.padron = padron;
+	}
+	
+	@Override
+	public void setNombre(String nombre) {
+		super.nombre = nombre;
+		super.id = 10;
+		System.out.println("padre: " + super.nombre);
+		System.out.println("hijo: " + this.nombre);
+		System.out.println("static: " + super.id);
+		System.out.println("static: " + Alumno.id);
+		System.out.println("static: " + this.id);
+		System.out.println(Object.class.getSimpleName());
+	}
+
+}
