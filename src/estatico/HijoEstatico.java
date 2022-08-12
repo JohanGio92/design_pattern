78711255b2fdb88094dd251f9afc0a092268c433
Johan Revilla <johan_revilla_92@hotmail.com>
2022-08-12 14:19:09 -0300

    add a comment a HijoEstatico.java

--- src/estatico/HijoEstatico.java
+++ src/estatico/HijoEstatico.java
@@ -1,9 +1,21 @@
 package estatico;
 
+import javax.management.remote.SubjectDelegationPermission;
+
 public class HijoEstatico extends PadreEstatico {
 
 	public HijoEstatico() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static void metodoA() {
+		PadreEstatico.metodoB();
+		PadreEstatico.metodoA();
+	}
+	
+	public static void metodoD() {
+		//no se puede heredar un atributo y metodo estatico privado
+		System.out.println("soy un metodo D + B: " + PadreEstatico.B);
 	}
 
 }
