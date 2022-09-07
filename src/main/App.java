7dd6a60f215a0272fbbe12c1d8b1bab7ae54372e
Johan Revilla <johan_revilla_92@hotmail.com>
2022-09-07 10:56:09 -0300

    finish flyweight apaw

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,29 @@
 package main;
 
+import flyweightAPAW.Factory;
+import flyweightAPAW.FlyweightConcrete;
+
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
+		Factory factory = new Factory();
+		
+		FlyweightConcrete[] flyweightConcrete = new FlyweightConcrete[5];
+		
+		flyweightConcrete[0] = factory.get("uno");
+		flyweightConcrete[1] = factory.get("dos");
+		flyweightConcrete[2] = factory.get("tres");
+		flyweightConcrete[3] = factory.get("dos");
+		flyweightConcrete[4] = factory.get("uno");
+		
+		for (int i = 0; i < flyweightConcrete.length; i++) {
+			System.out.println(flyweightConcrete[i].operation("---"));
+		}
+
+		for (int i = 0; i < flyweightConcrete.length; i++) {
+			System.out.println(flyweightConcrete[i].operation("<<"));
+		}
 	}
 
 }
